4e298b09d09b9672cbd10258210ac73b6d303bad
Shinobu Izumi <izumi@cis.sojo-u.ac.jp>
2016-08-16 20:26:43 +0900

    updated for BLENano 1.5

--- arduino/BLE_Ground_Sensor/BLE_Ground_Sensor.ino
+++ arduino/BLE_Ground_Sensor/BLE_Ground_Sensor.ino
@@ -1,11 +1,14 @@
 /*
   author Shinobu Izumi (Sojo University)
  */
+#include <BLE_API.h>
 
 #define TXRX_BUF_LEN                      20
 
-BLE                                      ble;
-Ticker                                    ticker;
+#define STATUS_CHECK_TIME                APP_TIMER_TICKS(1000, 0)
+
+BLEDevice                                      ble;
+static app_timer_id_t                    m_1s_timer_id; 
 
 //static uint16_t value=0;
 
@@ -77,109 +80,105 @@ void readSensorValues(){
   humid = A_val_humid * (500.0 / 330.0); // for 3.3V input
   tempC = A_val_temp / 1024.0  * 330.0;  // for 3.3V input
 
-  Serial1.print("Humid:");
-  Serial1.print(humid);
-  Serial1.print(":");
-  Serial1.print(A_val_humid);
+  Serial.print("Humid:");
+  Serial.print(humid);
+  Serial.print(":");
+  Serial.print(A_val_humid);
   
-  Serial1.print(",   ");
+  Serial.print(",   ");
   
-  Serial1.print("Temp:");
-  Serial1.print(tempC);
-  Serial1.print(":");
-  Serial1.print(A_val_temp);
-  Serial1.println("");
+  Serial.print("Temp:");
+  Serial.print(tempC);
+  Serial.print(":");
+  Serial.print(A_val_temp);
+  Serial.println("");
 }
 
 
-static void disconnectionCallBack(Gap::Handle_t handle, Gap::DisconnectionReason_t reason)
+static void disconnectionCallBack(void)
 {
-    Serial1.println("Disconnected ");
-    Serial1.println("Restart advertising ");
+    Serial.println("Disconnected ");
+    Serial.println("Restart advertising ");
     ble.startAdvertising();
 }
 
 // GATT call back handle
-void writtenHandle(const GattWriteCallbackParams *Handler)
+void writtenHandle(uint16_t charHandle)
 {
     uint8_t buf[TXRX_BUF_LEN];
     uint16_t bytesRead, index;
 
-    Serial1.println("Write Handle : ");
-    if (Handler->handle == characteristic1.getValueAttribute().getHandle())
+    Serial.println("Write Handle : ");
+    if (charHandle == characteristic1.getHandle())
     {
-        ble.readCharacteristicValue(characteristic1.getValueAttribute().getHandle(), buf, &bytesRead);
+        ble.readCharacteristicValue(characteristic1.getHandle(), buf, &bytesRead);
         for(byte index=0; index<bytesRead; index++)
         {
-            Serial1.print(buf[index], HEX);
+            Serial.print(buf[index], HEX);
         }
-        Serial1.println(" ");
+        Serial.println(" ");
     }
 }
 
 uint8_t buf[2];
 
 // Task handle
-void m_1s_handle(void)
+void m_1s_handle(void * p_context)
 {
-    Serial1.println("1s Loop ");
+    Serial.println("1s Loop ");
     readSensorValues();
 
     uint16_t iHumid = round(humid);
     memcpy(buf, &iHumid, 2);    
-    ble.updateCharacteristicValue(characteristic2.getValueAttribute().getHandle(), buf, 2);
-    ble.updateCharacteristicValue(characteristic3.getValueAttribute().getHandle(), buf, 2);
+    ble.updateCharacteristicValue(characteristic2.getHandle(), buf, 2);
+    ble.updateCharacteristicValue(characteristic3.getHandle(), buf, 2);
 
     
     uint16_t iTemp = round(tempC);
     memcpy(buf, &iTemp, 2);
-    ble.updateCharacteristicValue(characteristic4.getValueAttribute().getHandle(), buf, 2);
-    ble.updateCharacteristicValue(characteristic5.getValueAttribute().getHandle(), buf, 2);
+    ble.updateCharacteristicValue(characteristic4.getHandle(), buf, 2);
+    ble.updateCharacteristicValue(characteristic5.getHandle(), buf, 2);
 }
 
 
 void ble_setup() {
+  uint32_t err_code = NRF_SUCCESS;
   
-    ticker.attach(m_1s_handle, 1);
     // put your setup code here, to run once
     pinMode(D13, OUTPUT);
     
-    Serial1.println("Start BLE ");
+    Serial.println("Start BLE ");
     ble.init();
     ble.onDisconnection(disconnectionCallBack);
     ble.onDataWritten(writtenHandle);
+    
+    err_code = app_timer_create(&m_1s_timer_id, APP_TIMER_MODE_REPEATED, m_1s_handle );
+    APP_ERROR_CHECK(err_code);
+    err_code = app_timer_start(m_1s_timer_id, STATUS_CHECK_TIME, NULL);
+    APP_ERROR_CHECK(err_code);  
       
     // setup adv_data and srp_data
     ble.accumulateAdvertisingPayload(GapAdvertisingData::BREDR_NOT_SUPPORTED);
     ble.accumulateAdvertisingPayload(GapAdvertisingData::SHORTENED_LOCAL_NAME,
-                                     (const uint8_t *)"TXRX", sizeof("TXRX") - 1);
+                                     (const uint8_t *)"BLE_GS", sizeof("BLE_GS") - 1);
+//    ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LOCAL_NAME,
+//                                     (const uint8_t *)"BLE_GS", sizeof("BLE_GS") - 1);
     ble.accumulateAdvertisingPayload(GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS,
                                      (const uint8_t *)uart_base_uuid_rev, sizeof(uart_base_uuid_rev));
-                                     
-    ble.accumulateScanResponse(GapAdvertisingData::SHORTENED_LOCAL_NAME, 
-                                     (const uint8_t *)"hello", sizeof("hello") - 1);                        
-    ble.accumulateScanResponse(GapAdvertisingData::COMPLETE_LIST_128BIT_SERVICE_IDS,
-                                     (const uint8_t *)uart_base_uuid_rev, sizeof(uart_base_uuid_rev));     
-                
+                             
     // set adv_type
     ble.setAdvertisingType(GapAdvertisingParams::ADV_CONNECTABLE_UNDIRECTED);    
   // add service
     ble.addService(uartService);
-    // set device name
-    ble.setDeviceName((const uint8_t *)"BLE Ground Sensor");
-    // set tx power,valid values are -40, -20, -16, -12, -8, -4, 0, 4
-    ble.setTxPower(4);
 
     // set adv_interval, 100ms in multiples of 0.625ms.
     ble.setAdvertisingInterval(160);
     // set adv_timeout, in seconds
     ble.setAdvertisingTimeout(0);
-    // ger BLE stack version
-    Serial1.println( ble.getVersion() );
     // start advertising
     ble.startAdvertising();
 
-    Serial1.println("start advertising ");
+    Serial.println("start advertising ");
 }
 
 // the setup function runs once when you press reset or power the board
@@ -189,14 +188,14 @@ void setup() {
   pinMode(A_in_humid, INPUT);
   pinMode(A_in_temp, INPUT);
   
-  Serial1.begin(9600);
+  Serial.begin(9600);
 
   ble_setup();
 }
 
 // the loop function runs over and over again forever
 void loop() {
-  Serial1.println("=========");
+  Serial.println("=========");
   
   ble.waitForEvent();
   
